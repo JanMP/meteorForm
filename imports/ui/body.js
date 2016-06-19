eb794eeaea61f95946729c785ccaeadbf3f0518c
Jan Pilgenröder <pille@mac.com>
2016-06-20 01:07:03 +0200

    fix autoform

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,7 +1,12 @@
 import { Template } from 'meteor/templating';
 // import { ReactiveVar } from 'meteor/reactive-var';
 
-import '../api/coffee/coffee.js';
+import { Coffee } from '../api/coffee/coffee.js';
 import '../api/coffee/methods.js';
 
 import './body.html';
+
+// autoform wants either the collection in window scope or from a helper that returns the collection. Since global scope is not the way to go anymore (and wasn't a good idea in the first place) we use the helper.
+Template.body.helpers({
+  Coffee() { return Coffee; }
+})
